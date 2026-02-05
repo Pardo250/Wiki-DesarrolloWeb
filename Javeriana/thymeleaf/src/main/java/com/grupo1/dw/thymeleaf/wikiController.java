0e41ba65b282feb918c233e524c2376cb7b149fb
JuanGarzonT <jesgarzontgmail.com>
2026-02-05 17:51:14 -0500

    Avance

--- Javeriana/thymeleaf/src/main/java/com/grupo1/dw/thymeleaf/wikiController.java
+++ Javeriana/thymeleaf/src/main/java/com/grupo1/dw/thymeleaf/wikiController.java
@@ -1,5 +1,14 @@
 package com.grupo1.dw.thymeleaf;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class wikiController {
     
+    @GetMapping ("/wiki")
+    public String wiki(){
+        return "index";
+    }
+        
 }
